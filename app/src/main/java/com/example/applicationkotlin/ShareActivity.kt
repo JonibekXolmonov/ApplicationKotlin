8764439e4355317fbf27442f57e0845f7d1fd4ed
Perennial2002 <pendragon05082002@gmail.com>
2021-12-24 09:45:51 +0500

    updated

---
+++ app/src/main/java/com/example/applicationkotlin/ShareActivity.kt
@@ -0,0 +1,69 @@
+package com.example.applicationkotlin
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Parcelable
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+
+class ShareActivity : AppCompatActivity() {
+    private var imageView: ImageView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_share)
+
+        initView()
+        intentContent()
+    }
+
+    private fun initView() {
+        imageView = findViewById(R.id.img_share)
+    }
+
+    private fun intentContent() {
+        // Get intent, action and MIME type
+        val intent = intent
+        val action = intent.action
+        val type = intent.type
+        if (Intent.ACTION_SEND == action && type != null) {
+            if ("text/plain" == type) {
+                handleSendText(intent) // Handle text being sent
+            } else if (type.startsWith("image/")) {
+                handleSendImage(intent) // Handle single image being sent
+            }
+        } else if (Intent.ACTION_SEND_MULTIPLE == action && type != null) {
+            if (type.startsWith("image/")) {
+                handleSendMultipleImages(intent) // Handle multiple images being sent
+            }
+        } else {
+            // Handle other intents, such as being started from the home screen
+        }
+    }
+
+    private fun handleSendText(intent: Intent) {
+        val sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+        if (sharedText != null) {
+            // Update UI to reflect text being shared
+            Log.d("@@@", sharedText)
+            //textView?.text = sharedText
+        }
+    }
+
+    private fun handleSendImage(intent: Intent) {
+        val imageUri = intent.getParcelableExtra<Parcelable>(Intent.EXTRA_STREAM) as Uri?
+        if (imageUri != null) {
+            imageView?.setImageURI(imageUri)
+        }
+    }
+
+    private fun handleSendMultipleImages(intent: Intent) {
+        val imageUris = intent.getParcelableArrayListExtra<Uri>(Intent.EXTRA_STREAM)
+        if (imageUris != null) {
+            // Update UI to reflect multiple images being shared
+        }
+    }
+}
